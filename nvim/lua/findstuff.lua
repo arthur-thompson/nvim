09af10e03af54c8d6c80c8dd0f72c4b03892193f
Arthur Thompson <102389665+arthur-thompson@users.noreply.github.com>
2023-06-21 20:52:05 +1200

    Add files via upload

--- nvim/lua/findstuff.lua
+++ nvim/lua/findstuff.lua
@@ -2,3 +2,4 @@ vim.api.nvim_set_keymap('n', '<leader>ff', '<cmd>Telescope find_files<cr>', { no
 vim.api.nvim_set_keymap('n', '<leader>fg', '<cmd>Telescope live_grep<cr>',  { noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>fb', '<cmd>Telescope buffers<cr>',    { noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>fh', '<cmd>Telescope help_tags<cr>',  { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>fa', '<cmd>Telescope find_files hidden=true<cr>',  { noremap = true })
